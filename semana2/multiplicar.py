b8809f780fd62cd48eb633f5934fe3f94406d075
Jasiel <jasielromo2@gmail.com>
2026-05-30 10:43:21 -0600

    introduciendo multiplicar.py

---
+++ semana2/multiplicar.py
@@ -0,0 +1,6 @@
+# Multiplicación de dos números
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+
+resultado = num1 * num2
+print("El resultado de la multiplicación es:", resultado)
