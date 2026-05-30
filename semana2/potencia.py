4f73899fadbfef12d5b231d6227d75956281547d
Jasiel <jasielromo2@gmail.com>
2026-05-30 10:45:09 -0600

    introduciendo potencia.py

---
+++ semana2/potencia.py
@@ -0,0 +1,6 @@
+# Potencia de un número
+base = int(input("Ingresa la base: "))
+exponente = int(input("Ingresa el exponente: "))
+
+resultado = base ** exponente
+print("El resultado de la potencia es:", resultado)
